8c42a76f0f53ca940030afcf1dd9fbddeb673cb2
Riley Corzine <rileycorzine@gmail.com>
2024-11-09 13:56:56 -0500

    modified kernel & infra

---
+++ run_long_ctx/make_runs_nsys.py
@@ -0,0 +1,11 @@
+from run_long_ctx.clean_text import clean_text_to_prompt
+from run_long_ctx.clean_text import input_text
+import os
+os.environ["NSYS_NVTX_PROFILER_REGISTER_ONLY"] = "0"  # be sure to profile C++
+for paramcount in ['130m','370m']:
+   for ctx_len in [1000,2000,4000]:
+        cmd=f"""sudo /usr/local/cuda/bin/nsys profile --trace=cuda --force-overwrite=true --output={paramcount}_{ctx_len} \
+   $(which python3) benchmarks/benchmark_generation_mamba_simple.py \
+   --model-name "state-spaces/mamba-{paramcount}" --prompt "{clean_text_to_prompt(input_text,ctx_len)}" \
+   --topp 0.9 --temperature 0.7 --repetition-penalty 1.2"""
+        os.system(cmd)
